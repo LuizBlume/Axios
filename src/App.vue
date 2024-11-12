00cf91ae55797c81d18b3e6f6cab67688f349a3d
LuizBlume <luiz.fernando.251107@gmail.com>
2024-11-11 22:15:39 -0300

    CHORE: Add new functions on pinia, and tasks TvView.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,38 @@
-<script setup>
-  import { ref, onMounted } from 'vue';
-  import api from '@/plugins/axios';
-
-  const genres = ref([]);
-
-  onMounted(async () => {
-    const response = await api.get('genre/tv/list?language=pt-BR');
-    genres.value = response.data.genres;
-  });
-</script>
+<script setup></script>
 
 <template>
-  <div>
-    <h1>Gêneros de programas de TV</h1>
-    <ul>
-      <li v-for="genre in genres" :key="genre.id">
-        {{ genre.name }}
-      </li>
-    </ul>
-  </div>
+  <header>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/filmes">Filmes</router-link>
+      <router-link to="/tv">Programas de TV</router-link>
+    </nav>
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
+
+
+<style scoped>
+header {
+  height: 3rem;
+  display: flex;
+  background-color: black;
+  color: #fff;
+  font-size: 1.2rem;
+  padding-left: 2rem;
+}
+
+nav {
+  column-gap: 2rem;
+  margin-bottom: 0;
+  display: flex;
+  align-items: center;
+}
+
+nav a {
+  text-decoration: none;
+  color: #fff;
+}
+</style>
--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -1,22 +1,73 @@
 <script setup>
-  import { ref, onMounted } from 'vue';
-  import api from '@/plugins/axios';
+import { ref, onMounted } from 'vue';
+import api from '@/plugins/axios';
+import Loading from 'vue-loading-overlay';
 
-  const genres = ref([]);
+const isLoading = ref(false);
+const genres = ref([]);
+const tv = ref([]);
 
-  onMounted(async () => {
-    const response = await api.get('genre/tv/list?language=pt-BR');
-    genres.value = response.data.genres;
-  });
+const listTv = async (genreId) => {
+  isLoading.value = true;
+  const response = await api.get('discover/tv', {
+    params: {
+      with_genres: genreId,
+      language: 'pt-BR'
+    }
+  })
+  tv.value = response.data.results
+  isLoading.value = false;
+}
+
+onMounted(async () => {
+  const response = await api.get('genre/tv/list?language=pt-BR');
+  genres.value = response.data.genres;
+});
 </script>
 
 <template>
-  <div>
-    <h1>Gêneros de programas de TV</h1>
-    <ul>
-      <li v-for="genre in genres" :key="genre.id">
-        {{ genre.name }}
-      </li>
-    </ul>
+  <h1>Programas de TV</h1>
+  <ul class="genre-list">
+    <li v-for="genre in genres" :key="genre.id" @click="listTv(genre.id)" class="genre-item">
+      {{ genre.name }}
+    </li>
+  </ul>
+  <loading v-model:active="isLoading" is-full-page />
+  <div class="tv-list">
+    <div v-for="listaTv in tv" :key="listaTv.id" class="tv-card">
+      <img :src="`https://image.tmdb.org/t/p/w500${listaTv.poster_path}`" :alt="listaTv.name" />
+      <div class="tv-details">
+        <p class="tv-title">{{ listaTv.name }}</p>
+        <p class="tv-release-date">{{ listaTv.first_air_date }}</p>
+        <p class="tv-original-name">{{ listaTv.original_name }}</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.genre-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  padding: 0;
+}
+
+.genre-item {
+  background-color: #5d6424;
+  border-radius: 1rem;
+  padding: 0.5rem 1rem;
+  align-self: center;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+}
+
+.genre-item:hover {
+  cursor: pointer;
+  background-color: #7d8a2e;
+  box-shadow: 0 0 0.5rem #5d6424;
+}
+</style>
